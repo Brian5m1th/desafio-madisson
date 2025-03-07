2c41500f4afe098eb3b605e1e7389d150b246ca2
Brian Smith <briandiscbol@gmail.com>
2025-03-07 11:57:15 -0300

    initial commit

---
+++ src/main/java/com/manager/sermao/SermaoApplication.java
@@ -0,0 +1,13 @@
+package com.manager.sermao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SermaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SermaoApplication.class, args);
+	}
+
+}
